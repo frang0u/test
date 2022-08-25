89e44090ee9c7ea9b5fda87058b999f4e037f0d1
frang0u <49074374+frang0u@users.noreply.github.com>
2022-08-25 22:26:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def get_words():
     key = config["words_key"]
     region_url = "http://api.tianapi.com/caihongpi/index?key={}".format(key)
     response = get(region_url, headers=headers).json()
-    words = response["newslist"]["content"]
+    words = response[0]
     return words
  
 def get_birthday(birthday, year, today):
